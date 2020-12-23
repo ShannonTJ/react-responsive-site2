e05c8b678d15f1295d94cb8cb65ece4152ed5211
Shannon TJ <shannontj@shaw.ca>
2020-12-23 16:04:01 -0700

    start navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo>react</NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
