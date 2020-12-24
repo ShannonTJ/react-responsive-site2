4a4979c8b7bf38fb33195380650e04bf44eda9e1
Shannon TJ <shannontj@shaw.ca>
2020-12-23 17:44:53 -0700

    styling hero section and video bg

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Video from "../../videos/video.mp4";
+import { HeroContainer, HeroBg, VideoBg } from "./HeroElements";
+
+const HeroSection = () => {
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,6 +14,7 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
+      <HeroSection />
     </>
   );
 };
