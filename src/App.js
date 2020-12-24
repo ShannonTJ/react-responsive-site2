a886a3448ca51ee6566bba7bc67e685802dfb62c
Shannon TJ <shannontj@shaw.ca>
2020-12-23 17:03:16 -0700

    start sidebar component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SidebarContainer, Icon, CloseIcon } from "./SidebarElements";
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
