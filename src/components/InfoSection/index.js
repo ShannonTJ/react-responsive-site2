7e3d0bf86132876af2229c501297044af4002907
Shannon TJ <shannontj@shaw.ca>
2020-12-24 16:51:07 -0700

    creating infosection

---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button } from "react-scroll";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+} from "./InfoElements";
+
+const InfoSection = () => {
+  return (
+    <>
+      <InfoContainer id={id}>
+        <InfoWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>TopLine</TopLine>
+                <Heading>Heading</Heading>
+                <Subtitle>Subtitle</Subtitle>
+                <BtnWrap>
+                  <Button to="home">Button</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
+};
+
+export default InfoSection;
