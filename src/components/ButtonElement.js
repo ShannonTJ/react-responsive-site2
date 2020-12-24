7f738f555f2a364325514ff5fd3ad0ce8c234441
Shannon TJ <shannontj@shaw.ca>
2020-12-23 18:05:06 -0700

    create button element

---
+++ src/components/ButtonElement.js
@@ -0,0 +1,12 @@
+//react scroll link by default
+import styled from "styled-components";
+import { Link } from "react-scroll";
+
+export const Button = styled(Link)`
+  border-radius: 50px;
+  background: ${({ primary }) => (primary ? "#01bf71" : "#010606")};
+  white-space: nowrap;
+  padding: ${({ big }) => (big ? "14px 48px" : "12px 30px")};
+  color: ${({ dark }) => (dark ? "#010606" : "#fff")};
+  font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
+`;
