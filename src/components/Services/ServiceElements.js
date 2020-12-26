63d416de8f8185fb83c9dbbb67312bb626a4ccc5
Shannon TJ <shannontj@shaw.ca>
2020-12-26 14:53:51 -0700

    styling services

---
+++ src/components/Services/ServiceElements.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const ServicesContainer = styled.div`
+  height: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #010606;
+
+  @media screen and (max-width: 768px) {
+    height: 1100px;
+  }
+
+  @media screen and (max-width: 480px) {
+    height: 1300px;
+  }
+`;
+
+export const ServicesWrapper = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  grid-gap: 16px;
+  padding: 0 50px;
+
+  @media screen and (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    padding: 0 20px;
+  }
+`;
+
+export const ServicesCard = styled.div`
+  background: #fff;
+  display: flex;
+  flex-direction: flex-start;
+  align-items: center;
+  border-radius: 10px;
+  max-height: 340px;
+  padding: 30px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.02);
+    transition: 1ll 0.2s ease-in-out;
+    cursor: pointer;
+  }
+`;
