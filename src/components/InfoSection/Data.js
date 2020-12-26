2a99c1161b317f7e987ca745d2e90bab24001e61
Shannon TJ <shannontj@shaw.ca>
2020-12-25 23:12:28 -0700

    adding images

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObj1 = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: "images/svg-1.svg",
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
