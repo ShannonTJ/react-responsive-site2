da9800e38c61ca3ed49a83241561e4aa31cd1eae
Shannon TJ <shannontj@shaw.ca>
2020-12-26 16:06:16 -0700

    styling footer

---
+++ src/components/Footer/FooterElements.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const FooterContainer = styled.footer`
+  background-color: #101522;
+`;
+
+export const FooterWrapper = styled.div`
+  padding: 48px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1100px;
+  margin: 0 auto;
+`;
+
+export const FooterLinksContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 820px) {
+    padding-top: 32px;
+  }
+`;
+
+export const FooterLinksWrapper = styled.div`
+  display: flex;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
