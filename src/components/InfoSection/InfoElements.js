4af6cb558616951bef8f06813d629f466ba972cf
Shannon TJ <shannontj@shaw.ca>
2020-12-25 22:44:10 -0700

    styling infosection

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -61,3 +61,23 @@ export const TopLine = styled.p`
   text-transform: uppercase;
   margin-bottom: 16px;
 `;
+
+export const Heading = styled.h1`
+  margin-bottom: 24px;
+  font-size: 48px;
+  line-height: 1.1;
+  font-weight: 600;
+  color: ${({ lightText }) => (lightText ? "#f7f8fa" : "#010606")};
+
+  @media screen and (max-width: 480px) {
+    font-size: 32px;
+  }
+`;
+
+export const Subtitle = styled.p`
+  max-width: 440px;
+  margin-bottom: 35px;
+  font-size: 18px;
+  line-height: 24px;
+  color: ${({ darkText }) => (darkText ? "#010606" : "#fff")};
+`;
