ca063f8e5b2b52f730c1ec6a0f5546222553e4a9
Shannon TJ <shannontj@shaw.ca>
2020-12-26 15:08:36 -0700

    import svg into services component

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
   ServicesContainer,
   ServicesH1,
@@ -15,21 +16,21 @@ const Services = () => {
       <ServicesH1>Our Services</ServicesH1>
       <ServicesWrapper>
         <ServicesCard>
-          <ServicesIcon src={Icon1} />
+          <ServicesIcon src={"images/svg-5.svg"} />
           <ServicesH2>Reduce expenses</ServicesH2>
           <ServicesP>
             We help reduce your fees and increase your overall revenue
           </ServicesP>
         </ServicesCard>
         <ServicesCard>
-          <ServicesIcon src={Icon2} />
+          <ServicesIcon src={"images/svg-6.svg"} />
           <ServicesH2>Virtual Offices</ServicesH2>
           <ServicesP>
             You can access our platform online anywhere in the world
           </ServicesP>
         </ServicesCard>
         <ServicesCard>
-          <ServicesIcon src={Icon3} />
+          <ServicesIcon src={"images/svg-7.svg"} />
           <ServicesH2>Premium Benefits</ServicesH2>
           <ServicesP>
             Unlock our special membership card that returns 5% cash back
