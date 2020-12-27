6a7967c8224174b226ab7b7cda157b22bfc39264
Shannon TJ <shannontj@shaw.ca>
2020-12-26 17:33:06 -0700

    add social media section to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import {
+  FaTwitter,
+  FaYoutube,
+  FaInstagram,
+  FaLinkedin,
+  FaFacebook,
+} from "react-icons/fa";
+import {
   FooterContainer,
   FooterWrapper,
   FooterLinksContainer,
@@ -49,6 +56,31 @@ const Footer = () => {
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">react</SocialLogo>
+            <WebsiteRights>
+              react © {new Date().getFullYear()} all rights reserved
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrapper>
     </FooterContainer>
   );
